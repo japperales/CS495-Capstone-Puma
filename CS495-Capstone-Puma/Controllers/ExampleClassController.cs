f0f148c6dfe2f4a4fcf2829fda624e76870f20a9
John <japperales@gmial.com>
2019-11-07 16:55:18 -0500

    Added partially functioning post request

--- CS495-Capstone-Puma/Controllers/ExampleClassController.cs
+++ CS495-Capstone-Puma/Controllers/ExampleClassController.cs
@@ -1,6 +1,9 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mime;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CS495_Capstone_Puma.Controllers
@@ -15,23 +18,27 @@ namespace CS495_Capstone_Puma.Controllers
             return person;
         }
 
-        [HttpPost("[action]")]
-        public Person ConvertLoser()
+        [HttpPost]
+        [Route("[action]")]
+        [Consumes(MediaTypeNames.Application.Json)]
+        public  JsonResult ConvertLoser([FromBody] string name, [FromBody] int age, [FromBody] string job)
         {
-            return new Person("Ignore this for now", 0, "you heard me, ignore it");
+           Person newCoolGuy = new Person(name, age, job);
+           newCoolGuy.job = "Professional Cool Guy";
+           return Json(newCoolGuy);
         }
 
         public class Person
         {
-            public string Name { get; set; }
-            public int Age { get; set; }
-            public string Job { get; set; }
+            public string name { get; set; }
+            public int age { get; set; }
+            public string job { get; set; }
             
             public Person(string name, int age, string job)
             {
-                this.Name = name;
-                this.Age = age;
-                this.Job = job;
+                this.name = name;
+                this.age = age;
+                this.job = job;
             }
             
         }
