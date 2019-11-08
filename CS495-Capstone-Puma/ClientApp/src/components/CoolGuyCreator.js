993874552cd56a60d1a26cbc435ec70597ef6d65
John <japperales@gmial.com>
2019-11-07 22:39:40 -0500

    Actually fixed data not being sent to back end

--- CS495-Capstone-Puma/ClientApp/src/components/CoolGuyCreator.js
+++ CS495-Capstone-Puma/ClientApp/src/components/CoolGuyCreator.js
@@ -19,7 +19,7 @@ export class CoolGuyCreator extends Component{
               method: 'POST',
               headers: {
                   'Accept': 'application/json',
-                  'Content-Type': 'application/json',
+                  'Content-Type': 'application/json; charset=UTF-8',
               },
               body: JSON.stringify({
                   
