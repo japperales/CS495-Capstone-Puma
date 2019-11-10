842516c0f1d55a7f3ba7ff7cf44e081bc74734a3
John <japperales@gmial.com>
2019-11-09 23:11:53 -0500

    Example Page fully implemented

--- CS495-Capstone-Puma/ClientApp/src/components/CoolGuyCreator.js
+++ CS495-Capstone-Puma/ClientApp/src/components/CoolGuyCreator.js
@@ -4,17 +4,19 @@ export class CoolGuyCreator extends Component{
     
     constructor(props) {
         super(props);
-        this.state={name : "NULLNAME", job :"NULLJOB", age : -999999};
+        this.state={name : 'JP', job : 'Professional Coolest Guy', age : 22,
+                            inputName: '', inputJob: '', inputAge: null};
         /*fetch('api/ExampleClass/CoolGuyCreator')
             .then(response => response.json())
             .then(data => {
                 this.setState({ name: data.name, job : data.job, age : data.age});
             });*/
         this.SendLoser = this.SendLoser.bind(this);
-        
+        this.handleInputChange = this.handleInputChange.bind(this);
     }
 
-      SendLoser() {
+      SendLoser(event) {
+          event.preventDefault();
           fetch('api/ExampleClass/ConvertLoser', {
               method: 'POST',
               headers: {
@@ -23,38 +25,51 @@ export class CoolGuyCreator extends Component{
               },
               body: JSON.stringify({
                   
-                  name:'Cool King',
-                  age: 45,
-                  job : 'Burger Sommelier'
+                  name: this.state.inputName,
+                  age: this.state.inputAge,
+                  job: this.state.inputJob
               })
           }).then(response => response.json())
               .then(data => {
                   this.setState({ name: data.name, job : data.job, age : data.age});
               });
     }
+    
+    
         
+    handleInputChange(event){
+        
+        const target = event.target;
+        const value = event.target.value;
+        const name = target.name;
+    
+        this.setState({
+            [name]: value
+        });
+        
+    }
     
     render()
-    {       this.SendLoser();
+    {       
         return(<div>
             <p>Welcome to the cool guy creator! Make your cool guy by entering in your guy and watch him turn into a COOL guy!</p><br/>
             <p>Here we have: {this.state.name}, he is a: {this.state.job}, and he is: {this.state.age} years old.</p>
             
-            <form>
+            <form onSubmit={this.SendLoser}>
                 <label>Guy Name</label>
                 <br/>
-                <input type="text" name="Guy Name"/>
+                <input type="text" name="inputName" onChange={this.handleInputChange} value={this.state.inputName}/>
                 <br/>
                 <label>Guy Age</label>
                 <br/>
-                <input type="text" name="Guy Age"/>
+                <input type="number" name="inputAge" onChange={this.handleInputChange} value={this.state.inputAge} />
                 <br/>
                 <label>Guy Job</label>
                 <br/>
-                <input type="text" name="Guy Job"/>
+                <input type="text" name="inputJob" onChange={this.handleInputChange} value={this.state.inputJob}/>
                 <br/>
                 <br/>
-                <button >button?</button>
+                <input type="submit" value="Submit" />
             </form>
             </div>);
     }
