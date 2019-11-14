2c37100f2d7f21ab0b34208287b9ae5160c18953
ryland <rdbabusiak@bsu.edu>
2019-11-14 03:41:57 -0500

    JSON Deserialization Structures
    +DataStructures
        Added CS classes that reflect the JSON documents from Cheetah API Communication
        Nested classes for nested JSON trees
        Email and Phone share ContactMethod.cs class
    
    +UnitTests Project
        New Project containing UnitTesting classes
        xUnit dependent

---
+++ CS495-Capstone-Puma/DataStructure/Account/PerformanceExclusion.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace CS495_Capstone_Puma.DataStructure.Account
+{
+    public class PerformanceExclusion
+    {
+        [JsonProperty("PerformanceExclusionId")]
+        public long PerformanceExclusionId { get; set; }
+
+        [JsonProperty("InvestmentSegmentAllocationType")]
+        public string InvestmentSegmentAllocationType { get; set; }
+
+        [JsonProperty("AccountId")]
+        public long AccountId { get; set; }
+
+        [JsonProperty("AccountGroupId")]
+        public long AccountGroupId { get; set; }
+
+        [JsonProperty("AssetId")]
+        public long AssetId { get; set; }
+
+        [JsonProperty("PortfolioReportCategoryId")]
+        public long PortfolioReportCategoryId { get; set; }
+
+        [JsonProperty("IndustryId")]
+        public long IndustryId { get; set; }
+
+        [JsonProperty("AssetCategory")]
+        public string AssetCategory { get; set; }
+
+        [JsonProperty("AssetPrimaryClassType")]
+        public string AssetPrimaryClassType { get; set; }
+
+        [JsonProperty("IsGlobal")]
+        public bool IsGlobal { get; set; }
+
+        [JsonProperty("DateBeginning")]
+        public DateTimeOffset DateBeginning { get; set; }
+
+        [JsonProperty("DateEnding")]
+        public DateTimeOffset DateEnding { get; set; }
+
+        [JsonProperty("InstitutionIdentityRecordId")]
+        public long InstitutionIdentityRecordId { get; set; }
+
+        [JsonConstructor]
+        public PerformanceExclusion(long performanceExclusionId, string investmentSegmentAllocationType, long accountId, long accountGroupId, long assetId, long portfolioReportCategoryId, long industryId, string assetCategory, string assetPrimaryClassType, bool isGlobal, DateTimeOffset dateBeginning, DateTimeOffset dateEnding, long institutionIdentityRecordId)
+        {
+            PerformanceExclusionId = performanceExclusionId;
+            InvestmentSegmentAllocationType = investmentSegmentAllocationType;
+            AccountId = accountId;
+            AccountGroupId = accountGroupId;
+            AssetId = assetId;
+            PortfolioReportCategoryId = portfolioReportCategoryId;
+            IndustryId = industryId;
+            AssetCategory = assetCategory;
+            AssetPrimaryClassType = assetPrimaryClassType;
+            IsGlobal = isGlobal;
+            DateBeginning = dateBeginning;
+            DateEnding = dateEnding;
+            InstitutionIdentityRecordId = institutionIdentityRecordId;
+        }
+    }
+}
---
+++ CS495-Capstone-Puma/DataStructure/IdentityRecord/IdentityRecord.cs
@@ -0,0 +1,110 @@
+﻿﻿using System;
+ using System.Collections.Generic;
+ using Newtonsoft.Json;
+
+ namespace CS495_Capstone_Puma.DataStructure.IdentityRecord
+{
+    public class IdentityRecord
+    {
+        [JsonProperty("IdentityRecordId")]
+        public long IdentityRecordId { get; set; }
+
+        [JsonProperty("Code")]
+        public string Code { get; set; }
+
+        [JsonProperty("DisplayName")]
+        public string DisplayName { get; set; }
+
+        [JsonProperty("PayeeName")]
+        public string PayeeName { get; set; }
+
+        [JsonProperty("TaxIdStatusType")]
+        public string TaxIdStatusType { get; set; }
+
+        [JsonProperty("TaxId")]
+        public string TaxId { get; set; }
+
+        [JsonProperty("TaxIdType")]
+        public string TaxIdType { get; set; }
+
+        [JsonProperty("DomainModelClass")]
+        public string DomainModelClass { get; set; }
+
+        [JsonProperty("Comments")]
+        public string Comments { get; set; }
+
+        [JsonProperty("SalutationType")]
+        public string SalutationType { get; set; }
+
+        [JsonProperty("GenderType")]
+        public string GenderType { get; set; }
+
+        [JsonProperty("FirstNameLegalName")]
+        public string FirstNameLegalName { get; set; }
+
+        [JsonProperty("MiddleName")]
+        public string MiddleName { get; set; }
+
+        [JsonProperty("LastName")]
+        public string LastName { get; set; }
+
+        [JsonProperty("Title")]
+        public string Title { get; set; }
+
+        [JsonProperty("ContactName")]
+        public string ContactName { get; set; }
+
+        [JsonProperty("DateOfBirth")]
+        public DateTimeOffset DateOfBirth { get; set; }
+
+        [JsonProperty("DateOfDeath")]
+        public DateTimeOffset DateOfDeath { get; set; }
+
+        [JsonProperty("PhoneNumbers")]
+        public List<ContactMethod> PhoneNumbers { get; set; }
+
+        [JsonProperty("Emails")]
+        public List<ContactMethod> Emails { get; set; }
+
+        [JsonProperty("Addresses")]
+        public List<Address> Addresses { get; set; }
+
+        [JsonProperty("IdentityClassificationTypes")]
+        public List<string> IdentityClassificationTypes { get; set; }
+
+        [JsonProperty("IsActive")]
+        public bool IsActive { get; set; }
+
+        [JsonProperty("InstitutionIdentityRecordId")]
+        public long InstitutionIdentityRecordId { get; set; }
+
+        [JsonConstructor]
+        public IdentityRecord(long identityRecordId, string code, string displayName, string payeeName, string taxIdStatusType, string taxId, string taxIdType, string domainModelClass, string comments, string salutationType, string genderType, string firstNameLegalName, string middleName, string lastName, string title, string contactName, DateTimeOffset dateOfBirth, DateTimeOffset dateOfDeath, List<ContactMethod> phoneNumbers, List<ContactMethod> emails, List<Address> addresses, List<string> identityClassificationTypes, bool isActive, long institutionIdentityRecordId)
+        {
+            IdentityRecordId = identityRecordId;
+            Code = code;
+            DisplayName = displayName;
+            PayeeName = payeeName;
+            TaxIdStatusType = taxIdStatusType;
+            TaxId = taxId;
+            TaxIdType = taxIdType;
+            DomainModelClass = domainModelClass;
+            Comments = comments;
+            SalutationType = salutationType;
+            GenderType = genderType;
+            FirstNameLegalName = firstNameLegalName;
+            MiddleName = middleName;
+            LastName = lastName;
+            Title = title;
+            ContactName = contactName;
+            DateOfBirth = dateOfBirth;
+            DateOfDeath = dateOfDeath;
+            PhoneNumbers = phoneNumbers;
+            Emails = emails;
+            Addresses = addresses;
+            IdentityClassificationTypes = identityClassificationTypes;
+            IsActive = isActive;
+            InstitutionIdentityRecordId = institutionIdentityRecordId;
+        }
+    }
+}
