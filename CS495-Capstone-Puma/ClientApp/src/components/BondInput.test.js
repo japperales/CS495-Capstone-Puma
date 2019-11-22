531d6f301fb1ee961b0d0a2d3ce0f86d413eba29
John <japperales@gmial.com>
2019-11-22 03:56:02 -0500

    implemented Basic Componenent tests

---
+++ CS495-Capstone-Puma/ClientApp/src/components/BondInput.test.js
@@ -0,0 +1,17 @@
+﻿import {BondInput} from "./BondInput"
+import React from "react";
+it("Testing Jest", () => {
+    expect(1+1).toEqual(2);
+});
+
+it("Testing Jest 2", () => {
+    expect(2+2===4).toEqual(true);
+});
+
+it("BondInput Renders Correctly", () => {
+    const wrapper = shallow(
+        <BondInput />
+    );
+    
+    expect(wrapper).toMatchSnapshot();
+});
---
+++ CS495-Capstone-Puma/ClientApp/src/components/StockInput.test.js
@@ -0,0 +1,12 @@
+﻿import React from 'react';
+import {StockInput} from "./StockInput";
+
+
+it("PersonalInput Renders Correctly", () => {
+    const wrapper = shallow(
+        <StockInput />
+    );
+
+    expect(wrapper).toMatchSnapshot();
+
+});
