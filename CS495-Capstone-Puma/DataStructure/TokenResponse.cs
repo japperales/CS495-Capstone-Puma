40018fcf902473b152e40060279c521ce63839e7
John <japperales@gmial.com>
2020-01-13 00:57:42 -0500

    Added (untested) getAccessToken to CheetahHandler

---
+++ CS495-Capstone-Puma/DataStructure/TokenResponse.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json;
+
+namespace CS495_Capstone_Puma.DataStructure
+{
+    public class TokenResponse
+    {
+        [JsonProperty("Jwt")] public string Jwt { get; set; }
+        
+        [JsonProperty("ExpiresIn")] public int ExpiresIn { get; set; }
+        
+        [JsonProperty("IssuedDateTime")] public string IssuedDateTime { get; set; }
+        
+        [JsonProperty("ExpirationDateTime")] public string ExpirationDateTime { get; set; }
+    }
+}
---
+++ CS495-Capstone-Puma/Model/FlurlTests/PumaControllerTests.cs
@@ -0,0 +1,19 @@
+﻿using CS495_Capstone_Puma.Model;
+using Flurl.Http.Testing;
+
+namespace CS495_Capstone_Puma.Controllers.FlurlTests
+{
+    public class PumaControllerTests
+    {
+        /*[Test]
+        public void TestGetAccessToken()
+        {
+            using (var httpTest = new HttpTest())
+            {
+                CheetahHandler.GetAccessToken();
+                httpTest.
+            }
+        }*/
+        
+    }
+}
