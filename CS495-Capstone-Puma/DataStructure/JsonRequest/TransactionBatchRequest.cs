d0247beb2f8ae6a459b737ab0c3cc6232abd175d
ryland <rdbabusiak@bsu.edu>
2020-01-14 05:03:12 -0500

    ֍ Maɴic, Total Sc࿊pe ʘverhaul ֍
    Reusing pre-made account pool
    
    Adjusted Cheetah communication to limit only to transactions and trades
    Refactored data structures to contain only what is necessary
    Split into Request and Response directories
    
     ⌦ Still To Be Completed ⌫
       ❧ TransactionResponse.cs
       ❧ TradeResponse.cs
       ❧ Adjust PumaController to work with new behavior
       ❧ Assets lookup
       ❧ Trade Filtering
       ❧ Trade Analysis
       ❧ Portfolio Management

---
+++ CS495-Capstone-Puma/DataStructure/JsonRequest/TransactionBatchRequest.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+
+namespace CS495_Capstone_Puma.DataStructure.JsonRequest
+{
+    public class TransactionBatchRequest
+    {
+        [JsonProperty("InstitutionRecordId")]
+        public int institutionRecordId { get; }
+        
+        [JsonProperty("Code")]
+        public string code { get; }
+        
+        [JsonProperty("Name")]
+        public string name { get; }
+
+        public TransactionBatchRequest()
+        {
+            institutionRecordId = 1;
+            code = "123456789";
+            name = "PumaApi";
+        }
+    }
+}
