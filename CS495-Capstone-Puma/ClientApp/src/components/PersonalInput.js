6b2acdbb834068a01e8153c73121739f3e95564c
Tommy-cmd <thomasmferrigan@gmail.com>
2020-01-14 10:24:07 -0500

    redesign for the PersonalInput.js tab

--- CS495-Capstone-Puma/ClientApp/src/components/PersonalInput.js
+++ CS495-Capstone-Puma/ClientApp/src/components/PersonalInput.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import M from 'materialize-css';
 let state ={
     inputFirstName: null,
     inputMiddleName: null,
@@ -8,8 +8,14 @@ let state ={
     inputEmailAddress:null
 };
 
+
 export class PersonalInput extends React.Component{
-    
+
+    componentDidMount() {
+        console.log("component did mount");
+        M.AutoInit();
+    }
+
     constructor(props){
         super(props);
         this.state = state;
@@ -37,43 +43,47 @@ export class PersonalInput extends React.Component{
             <div>
                 <h3>Account Holder Information</h3> 
                 <br />
-                
+                <div className = "row">
+                <div className = "input-field col s6">
                 <label>First Name</label>
-                <br />
                 <input type="text" name="inputFirstName" onChange={this.handleInputChange} value={this.state.inputFirstName}/>
-                <br />
-                <br />
+                </div>
+                </div>
                 
+                <div className = "row">
+                <div className = "input-field col s6">
                 <label>Middle Name</label>
-                <br />
                 <input type="text" name="inputMiddleName" onChange={this.handleInputChange} value={this.state.inputMiddleName}/>
-                <br />
-                <br />
+                </div>
+                </div>
                 
+                <div className = "row">
+                <div className = "input-field col s6">
                 <label>Last Name</label>
-                <br />
                 <input type="text" name="inputLastName" onChange={this.handleInputChange} value={this.state.inputLastName}/>
-                <br />
-                <br />
+                </div>
+                </div>
                 
-                <label>Honorific</label>
-                <br />
+                <div className="row">
+                <div className = "input-field col s6">
                 <select name="inputHonorific" onChange={this.handleInputChange}  value={this.state.inputHonorific}>
+                    <option value="" disabled selected> </option>
                     <option value="Mr">Mr</option>
                     <option value="Mrs">Mrs</option>
                     <option value="Ms">Ms</option>
                     <option value="Dr">Dr</option>
-                    <option value="" selected="selected"> </option>
+                    
                 </select>
-                <br />
-                <br />
+                    <label>Honorific</label>
+                </div>
+                </div>
                 
+                <div className = "row">
+                <div className = "input-field col s6">
                 <label>Email</label>
-                <br />
                 <input type="text" name="inputEmailAddress" onChange={this.handleInputChange} value={this.state.inputEmailAddress}/>
-                <br />
-                <br />
-                <hr />
+                </div>
+                </div>
             </div>
         );
     }
