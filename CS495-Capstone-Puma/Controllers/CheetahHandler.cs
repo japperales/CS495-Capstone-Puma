02a33725b3f922537451023e7cbccb0a49735fe6
John <japperales@gmial.com>
2020-01-15 06:46:42 -0500

    Completed retrieving and formatting initial portfolio AND retrieving and formatting the relevant trade proposals.

--- CS495-Capstone-Puma/Controllers/CheetahHandler.cs
+++ CS495-Capstone-Puma/Controllers/CheetahHandler.cs
@@ -10,6 +10,7 @@ using CS495_Capstone_Puma.DataStructure;
 using CS495_Capstone_Puma.DataStructure.NameAndAddress;
  using CS495_Capstone_Puma.DataStructure.ResponseShards;
  using Flurl.Http;
+ using Microsoft.Extensions.Logging;
  using Newtonsoft.Json.Linq;
 
  namespace CS495_Capstone_Puma.Controllers
@@ -18,7 +19,7 @@ using CS495_Capstone_Puma.DataStructure.NameAndAddress;
     {
 
         //Coordinates the POST and GET HttpRequests required by the process.
-        public async Task<UIObject> PostAndReceive(IdentityRecord identityRecord, Account account, List<Asset> assets)
+        public async Task<Object[]> PostAndReceive(IdentityRecord identityRecord, Account account, List<Asset> assets)
         {
             //POST Authentication
             string accessToken = GetAccessToken().Result.Jwt;
@@ -26,7 +27,17 @@ using CS495_Capstone_Puma.DataStructure.NameAndAddress;
             //POST IdentityRecord & Account async
 
             IList<HoldingsShard> originalPortfolio = RetrieveOriginalPortfolio(accessToken).Result;
+
+
+            IList<TradeShard> tradeProposal = RetrieveTradeProposal(accessToken).Result;
             
+            Object[] responseArray = new Object[4];
+            responseArray[0] = originalPortfolio;
+            responseArray[1] = tradeProposal;
+            responseArray[2] = 484934.46;
+            responseArray[3] = 5891.01;
+
+            return responseArray;
             //PostIdentityRecord(identityRecord);
             //PostAccount(account);
             
@@ -98,20 +109,93 @@ using CS495_Capstone_Puma.DataStructure.NameAndAddress;
             
             return response;
         }
-
-        public static async Task<IList<HoldingsShard>> RetrieveOriginalPortfolio(string jwt)
+        
+        private static async Task<IList<HoldingsShard>> RetrieveOriginalPortfolio(string jwt)
         {
             var response = await "https://asctrustv57webapi.accutech-systems.net/api/v6/Accounts/1/Holdings"
                 .WithOAuthBearerToken(jwt)
                 .GetStringAsync();
 
             IList<HoldingsShard> holdings = ConvertHoldingTokenList(response);
-            
-            return holdings;
+            IList<HoldingsShard> namedHoldings = AddAssetNamesToHoldings(holdings, jwt);
+            return namedHoldings;
         }
 
-        private static IList<HoldingsShard> ConvertHoldingTokenList(string jsonHoldingsString)
+        private static async Task<IList<TradeShard>> RetrieveTradeProposal(string jwt)
         {
+            var response = await "https://asctrustv57webapi.accutech-systems.net/Api/v6/Trades?AccountId=1"
+                .WithOAuthBearerToken(jwt)
+                .GetStringAsync();
+            
+            IList<TradeShard> tradeList = ConvertTradeTokenList(response);
+            tradeList = FilterTradesByTradeBatchId(0, tradeList);
+            IList<TradeShard> namedTradeList = AddAssetNamesToTrades(tradeList, jwt);
+            
+            return namedTradeList;
+        }
+
+        private static IList<TradeShard> ConvertTradeTokenList(string response)
+        {
+            JArray parsedTrades = JArray.Parse(response);
+            IList<JToken> tradeTokens = parsedTrades.Children().ToList();
+            IList<TradeShard> tradeList = new List<TradeShard>();
+            foreach (JToken tradeToken in tradeTokens)
+            {
+                TradeShard tradeShard = tradeToken.ToObject<TradeShard>();
+                tradeList.Add(tradeShard);
+            }
+            return tradeList;
+        }
+
+        private static IList<TradeShard>  FilterTradesByTradeBatchId(int tradeBatchId, IList<TradeShard> tradeList)
+        {
+            for (int i = 0; i < tradeList.Count; i++)
+            {
+                if (tradeList[i].TradeBlockId != tradeBatchId)
+                {
+                    tradeList.Remove(tradeList[i]);
+                }
+            }
+
+            return tradeList;
+        }
+
+        private static IList<HoldingsShard> AddAssetNamesToHoldings(IList<HoldingsShard> holdingsShards, string jwt)
+        {
+            foreach (HoldingsShard holding in holdingsShards)
+            {
+                holding.AssetName = GetHoldingAssetName(holding.AssetId, jwt).Result;
+                Console.WriteLine(holding.AssetName);
+            }
+
+            return holdingsShards;
+        }
+        
+        private static IList<TradeShard> AddAssetNamesToTrades(IList<TradeShard> tradeShards, string jwt)
+        {
+            foreach (TradeShard tradeShard in tradeShards)
+            {
+                tradeShard.AssetName = GetHoldingAssetName(tradeShard.AssetId, jwt).Result;
+                Console.WriteLine(tradeShard.AssetName);
+            }
+
+            return tradeShards;
+        }
+        
+        private static async Task<string> GetHoldingAssetName(int assetId, string jwt)
+        {
+            var response = await ("https://asctrustv57webapi.accutech-systems.net/api/v6/Assets?AssetId=" + assetId)
+                .WithOAuthBearerToken(jwt)
+                .GetStringAsync();
+            JArray parsedResponseArray = JArray.Parse(response);
+            var unarrayedParsedResponse = parsedResponseArray[0];
+            JToken responseName = unarrayedParsedResponse["Issuer"];
+            string nameString = responseName.ToString();
+            return nameString;
+        }
+        
+        private static IList<HoldingsShard> ConvertHoldingTokenList(string jsonHoldingsString)
+        {    
             IList<HoldingsShard> holdingShardList = new List<HoldingsShard>();
 
             JArray parsedResponse = JArray.Parse(jsonHoldingsString);
@@ -122,9 +206,10 @@ using CS495_Capstone_Puma.DataStructure.NameAndAddress;
             {
                 HoldingsShard holdingShard = holdingToken.ToObject<HoldingsShard>();
                 IList<JToken> lotTokenList = holdingToken["Lots"].Children().ToList();
-                holdingShard.LotList = ConvertLotTokenList(lotTokenList);
-                Console.WriteLine(holdingShard.AssetId);
+                IList <LotShard> LotList = ConvertLotTokenList(lotTokenList);
+                holdingShard.CalculateFieldsFromLots(LotList);
                 holdingShardList.Add(holdingShard);
+                
             }
 
             return holdingShardList;
@@ -139,8 +224,10 @@ using CS495_Capstone_Puma.DataStructure.NameAndAddress;
                 LotShard lotObject = lotToken.ToObject<LotShard>();
                 lotList.Add(lotObject);
             }
-
+            
             return lotList;
         }
+
     }
+    
 }
