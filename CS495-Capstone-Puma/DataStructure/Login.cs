de3f1d2fe2269f3e0cc1fba27c87a969f7d5c9e9
ryland <rdbabusiak@bsu.edu>
2020-01-17 16:17:21 -0500

    Abstracted login to a .gitignored JSON file
    Removed hard-coding of AssetInputs
    
    Receiving assetInputs from Body of POST request

---
+++ CS495-Capstone-Puma/DataStructure/Login.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json;
+
+namespace CS495_Capstone_Puma.DataStructure
+{
+    public class Login
+    {
+        [JsonProperty("x-api-key")]
+        public string XApiKey { get; set; }
+        
+        [JsonProperty("username")]
+        public string Username { get; set; }
+        
+        [JsonProperty("password")]
+        public string Password { get; set; }
+
+        public Login()
+        {
+            XApiKey = "";
+            Username = "";
+            Password = "";
+        }
+
+        [JsonConstructor]
+        public Login(string xApiKey, string username, string password)
+        {
+            XApiKey = xApiKey;
+            Username = username;
+            Password = password;
+        }
+    }
+}
