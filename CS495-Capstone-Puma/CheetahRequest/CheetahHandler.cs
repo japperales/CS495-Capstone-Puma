4f09b4c8910da29fcee9c862f4cc26acd2e5d71b
ryland <rdbabusiak@bsu.edu>
2020-01-31 18:10:57 -0500

    Abstracted CheetahHandler into a set of individual static classes. Handler now functions as a middleman role, preparing data and holding configuration data. The Cheetah Api calls are moved to respective classes

---
+++ CS495-Capstone-Puma/CheetahRequest/CheetahHandler.cs
@@ -0,0 +1,141 @@
+ using System;
+ using System.Collections.Generic;
+ using System.IO;
+ using System.Threading.Tasks;
+ using CS495_Capstone_Puma.DataStructure;
+ using CS495_Capstone_Puma.DataStructure.JsonRequest;
+ using CS495_Capstone_Puma.DataStructure.JsonResponse;
+ using CS495_Capstone_Puma.DataStructure.JsonResponse.Asset;
+ using CS495_Capstone_Puma.DataStructure.ResponseShards;
+ using Newtonsoft.Json;
+
+ namespace CS495_Capstone_Puma.Model 
+  {
+    public class CheetahHandler
+    {
+        private readonly CheetahConfig _cheetahConfig = LoadApiConfig();
+        private readonly ProposalGet _proposalGet = new ProposalGet();
+
+        public static CheetahConfig LoadApiConfig()
+        {
+            using (StreamReader file = File.OpenText(@".\\cheetah-config.json"))
+            {
+                JsonSerializer serializer = new JsonSerializer();
+                CheetahConfig cheetahConfig = (CheetahConfig) serializer.Deserialize(file, typeof(CheetahConfig));
+                return cheetahConfig;
+            }
+        }
+        
+        //Coordinates Login (bearerToken retrieval) POST
+        public string PostLogin(Login login)
+        {
+            try
+            {
+                TokenResponse bearerToken = LoginPost.PostAccessToken(_cheetahConfig, login).Result;
+                return bearerToken.Jwt;
+            }
+            //If the login is incorrect, an exception is thrown and the login is not accepted
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return "badLogin";
+            }
+        }
+        
+        //Coordinates Asset/Transaction POST
+        public async Task<string> PostAssets(string bearerToken, List<AssetInput> assets)
+        {
+            try
+            {
+                //Create new Transaction Batch
+                int batchId = AssetPost.PostTransactionBatch(_cheetahConfig, bearerToken, new TransactionBatchRequest()).Result.transactionBatchId;
+                
+                //Turn asset list into a list of transactions to POST
+                List<TransactionRequest> transactions = AssembleTransactions(bearerToken, batchId, assets);
+                
+                //POST each transaction
+                foreach (TransactionRequest transaction in transactions)
+                {
+                    await AssetPost.PostTransaction(_cheetahConfig, bearerToken, transaction);
+                }
+                
+                await AssetPost.PostTransactionBatchProcessor(_cheetahConfig, bearerToken, batchId, false);
+                await AssetPost.PostTransactionBatchProcessor(_cheetahConfig, bearerToken, batchId, true);
+
+                return "PostAssets Successful";
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return "PostAssets Error";
+            }
+        }
+        
+        //Coordinates Proposed Trade GET
+        public Object[] GetTradeProposal(string bearerToken, string accountId)
+        {
+            //await getTrades(bearerToken, accountId);
+                    
+            IList<HoldingsShard> originalPortfolio = _proposalGet.RetrieveOriginalPortfolio(_cheetahConfig, bearerToken, accountId).Result;
+                    
+            IList<TradeShard> tradeProposal = _proposalGet.RetrieveTradeProposal(_cheetahConfig, bearerToken, accountId).Result;
+                    
+            Object[] responseArray= new Object[4];
+            responseArray[0] = originalPortfolio;
+            responseArray[1] = tradeProposal;
+            responseArray[2] = 484934.46;
+            responseArray[3] = 5891.01;
+            
+            return responseArray;
+        }
+
+        //Assemble list of Transactions from Assets list
+        private List<TransactionRequest> AssembleTransactions(string bearerToken, int batchId, List<AssetInput> assets)
+        {
+            //Populate Dictionary of assets from Cheetah with a GET request
+            Dictionary<AssetIdentifier, int> allAssets = PopulateAssetsDictionary(bearerToken);
+            
+            //Instantiate TransactionRequest List
+            List<TransactionRequest> transactionRequests = new List<TransactionRequest>();
+            
+            //for each asset in assets, check for entry in Dict
+            foreach (AssetInput asset in assets)
+            {
+                try
+                {
+                    int assetId = allAssets[asset.assetIdentifier];
+                    
+                    //Once found, build request and POST to the batch
+                    transactionRequests.Add(new TransactionRequest(
+                        534, batchId, 26, 19, assetId, asset.units));
+                }
+                catch (KeyNotFoundException e)
+                {
+                    Console.WriteLine(e);
+                    Console.WriteLine("Key not found");
+                }
+            }
+            return transactionRequests;
+        }
+
+        //Dictionary to match assets to internal id
+        private Dictionary<AssetIdentifier, int> PopulateAssetsDictionary(string bearerToken)
+        {
+            Dictionary<AssetIdentifier, int> assetsDictionary =
+                new Dictionary<AssetIdentifier, int>(new AssetIdentifier.AssetEqualityComparer());
+            
+            List<AssetLookupResponse> assetLookupResponses = Lookup.GetLookupAssets(_cheetahConfig, bearerToken).Result.items;
+            
+            foreach (AssetLookupResponse assetLookupResponse in assetLookupResponses)
+            {
+                try
+                {
+                    assetsDictionary.Add(assetLookupResponse.value, Int32.Parse(assetLookupResponse.id));
+                }
+                catch (ArgumentException){}
+            }
+            return assetsDictionary;
+        }
+    }
+}
---
+++ CS495-Capstone-Puma/CheetahRequest/ProposalGet.cs
@@ -0,0 +1,132 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CS495_Capstone_Puma.DataStructure;
+using CS495_Capstone_Puma.DataStructure.ResponseShards;
+using Flurl.Http;
+using Newtonsoft.Json.Linq;
+
+namespace CS495_Capstone_Puma.Model
+{
+    public class ProposalGet
+    {
+
+        public async Task<IList<HoldingsShard>> RetrieveOriginalPortfolio(CheetahConfig cheetahConfig, string bearerToken, string accountId)
+        {
+            var response = await (cheetahConfig.ApiUrlRoot + "Accounts/" + accountId + "/Holdings")
+                .WithOAuthBearerToken(bearerToken)
+                .GetStringAsync();
+        
+            IList<HoldingsShard> holdings = ConvertHoldingTokenList(response);
+            IList<HoldingsShard> namedHoldings = AddAssetNamesToHoldings(cheetahConfig, holdings, bearerToken);
+            return namedHoldings;
+        }
+
+        public async Task<IList<TradeShard>> RetrieveTradeProposal(CheetahConfig cheetahConfig, string bearerToken, string accountId)
+        {
+            var response = await (cheetahConfig.ApiUrlRoot + "Trades?AccountId=" + accountId)
+                .WithOAuthBearerToken(bearerToken)
+                .GetStringAsync();
+            
+            IList<TradeShard> tradeList = ConvertTradeTokenList(response);
+            tradeList = FilterTradesByTradeBatchId(0, tradeList);
+            IList<TradeShard> namedTradeList = AddAssetNamesToTrades(cheetahConfig, tradeList, bearerToken);
+            
+            return namedTradeList;
+        }
+        
+        private static IList<TradeShard> ConvertTradeTokenList(string response)
+        {
+            JArray parsedTrades = JArray.Parse(response);
+            IList<JToken> tradeTokens = parsedTrades.Children().ToList();
+            IList<TradeShard> tradeList = new List<TradeShard>();
+            foreach (JToken tradeToken in tradeTokens)
+            {
+                TradeShard tradeShard = tradeToken.ToObject<TradeShard>();
+                tradeList.Add(tradeShard);
+            }
+            return tradeList;
+        }
+
+        private static IList<TradeShard>  FilterTradesByTradeBatchId(int tradeBatchId, IList<TradeShard> tradeList)
+        {
+            for (int i = 0; i < tradeList.Count; i++)
+            {
+                if (tradeList[i].TradeBlockId != tradeBatchId)
+                {
+                    tradeList.Remove(tradeList[i]);
+                }
+            }
+
+            return tradeList;
+        }
+
+        private IList<HoldingsShard> AddAssetNamesToHoldings(CheetahConfig cheetahConfig, IList<HoldingsShard> holdingsShards, string bearerToken)
+        {
+            foreach (HoldingsShard holding in holdingsShards)
+            {
+                holding.AssetName = GetHoldingAssetName(cheetahConfig, holding.AssetId, bearerToken).Result;
+                
+            }
+
+            return holdingsShards;
+        }
+        
+        private IList<TradeShard> AddAssetNamesToTrades(CheetahConfig cheetahConfig, IList<TradeShard> tradeShards, string bearerToken)
+        {
+            foreach (TradeShard tradeShard in tradeShards)
+            {
+                tradeShard.AssetName = GetHoldingAssetName(cheetahConfig, tradeShard.AssetId, bearerToken).Result;
+                
+            }
+
+            return tradeShards;
+        }
+        
+        private async Task<string> GetHoldingAssetName(CheetahConfig cheetahConfig, int assetId, string bearerToken)
+        {
+            string response = await (cheetahConfig.ApiUrlRoot + "Assets?AssetId=" + assetId)
+                .WithOAuthBearerToken(bearerToken)
+                .GetStringAsync();
+            JArray parsedResponseArray = JArray.Parse(response);
+            JToken unarrayedParsedResponse = parsedResponseArray[0];
+            JToken responseName = unarrayedParsedResponse["Issuer"];
+            string nameString = responseName.ToString();
+            return nameString;
+        }
+        
+        private IList<HoldingsShard> ConvertHoldingTokenList(string jsonHoldingsString)
+        {    
+            IList<HoldingsShard> holdingShardList = new List<HoldingsShard>();
+
+            JArray parsedResponse = JArray.Parse(jsonHoldingsString);
+            JToken unarrayedHoldingsJson = parsedResponse[0];
+            IList<JToken> holdingTokenList = unarrayedHoldingsJson["Holdings"].Children().ToList();
+            
+            foreach (JToken holdingToken in holdingTokenList)
+            {
+                HoldingsShard holdingShard = holdingToken.ToObject<HoldingsShard>();
+                IList<JToken> lotTokenList = holdingToken["Lots"].Children().ToList();
+                IList <LotShard> lotList = ConvertLotTokenList(lotTokenList);
+                holdingShard.CalculateFieldsFromLots(lotList);
+                holdingShardList.Add(holdingShard);
+                
+            }
+
+            return holdingShardList;
+        }
+
+        private IList<LotShard> ConvertLotTokenList(IList<JToken> lotTokenList)
+        {
+            IList<LotShard> lotList = new List<LotShard>();
+            
+            foreach (JToken lotToken in lotTokenList)
+            {
+                LotShard lotObject = lotToken.ToObject<LotShard>();
+                lotList.Add(lotObject);
+            }
+            
+            return lotList;
+        }
+    }
+}
