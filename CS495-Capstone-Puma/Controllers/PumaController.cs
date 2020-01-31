f0f53b481a66d256d4d0230821279ccef08e70c1
John <japperales@gmial.com>
2020-01-31 15:04:15 -0500

    Added Cheetah Config and current PumaController and Startup.cs

--- CS495-Capstone-Puma/Controllers/PumaController.cs
+++ CS495-Capstone-Puma/Controllers/PumaController.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using CS495_Capstone_Puma.DataStructure;
 using CS495_Capstone_Puma.DataStructure.JsonResponse;
 using CS495_Capstone_Puma.DataStructure.JsonResponse.Asset;
+using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace CS495_Capstone_Puma.Controllers
-{    
+{   [EnableCors("AllowAnyOrigin")]
     [Route("api/[controller]")]
     public class PumaController : Controller 
     
@@ -21,23 +23,19 @@ namespace CS495_Capstone_Puma.Controllers
             //CheetahHandler cheetah = new CheetahHandler();
 
             //Perform POSTs, preserving the bearerToken
-            jwt =
-                "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJVc2VyR3VpZCI6IjI3ZTNmNzM3LTRiMmItNGJmNC05ZWQ3LTJmMDJkNjQzN2IzNiIsImh0dHA6Ly9zY2hlbWFzLnhtbHNvYXAub3JnL3dzLzIwMDUvMDUvaWRlbnRpdHkvY2xhaW1zL25hbWUiOiJyYmFidXNpYWsiLCJleHAiOjE1ODAyNTQxNTIsImlzcyI6Imh0dHBzOi8vd3d3LnRydXN0YXNjLmNvbS8iLCJhdWQiOiJodHRwczovL3d3dy50cnVzdGFzYy5jb20vIn0.5oaSPwCZZ2epUjeNSJX8j8De9EyM9azwlE09aiLqabM";
             //string bearerToken = cheetah.PostTransactions(assetInputs, jwt).Result;
 
            Object[] returnJson = cheetah.getTradeProposal(jwt, "1").Result;
-            
+            Console.WriteLine("Return JSON is: " + JsonConvert.SerializeObject(returnJson));
             //Serializes cheetah response into data structure understood by the frontend & returns that object as JSON
             return Json(returnJson);
         }
 
         [HttpPost("PostLogin")]
-        public JsonResult RetrieveToken([FromBody] Login login)
-        {    login= new Login();
-            login.Username = "***REMOVED***";
-            login.Password = "***REMOVED***";
+        [EnableCors("AllowAnyOrigin")]
+        public  JsonResult RetrieveToken([FromBody] Login login)
+        {
             TokenResponse tokenResponse = cheetah.PostAccessToken(login).Result;
-            Console.WriteLine("Token for postlogin is: " + tokenResponse.Jwt);
             return Json(tokenResponse);
         }
     }
